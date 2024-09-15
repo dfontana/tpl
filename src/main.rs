091b2d09655ffd84b999c907f7b76fd04e73febe
Dylan Fontana <dyfontana@gmail.com>
2024-09-14 22:35:49 -0400

    Lifecycles wired, needs impl

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use anyhow::{bail, Context};
 use clap::{Parser, Subcommand};
 use clap_verbosity_flag::{ErrorLevel, Level, Verbosity};
 use directories::{ProjectDirs, UserDirs};
-use minijinja::{path_loader, Environment};
+use minijinja::value::Object;
+use minijinja::Environment;
 use notify_debouncer_full::notify::{Error, INotifyWatcher, RecursiveMode, Watcher};
 use notify_debouncer_full::{new_debouncer, DebouncedEvent, Debouncer, FileIdMap};
 use parking_lot::{deadlock, Mutex, RwLock};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::fmt::Debug;
 use std::io::Write;
 use std::path::{Path, PathBuf};
@@ -45,7 +46,9 @@ struct Config {
 #[derive(Debug, Serialize, Deserialize)]
 struct Tpl {
     // TODO: Code assumes files, perhaps future update allows directories (src && dst == dirs, relative copies)
+    #[serde(deserialize_with = "resolve_tilde_s")]
     src: PathBuf,
+    #[serde(deserialize_with = "resolve_tilde_s")]
     dst: PathBuf,
 }
 
@@ -53,7 +56,6 @@ const APP_NAME: &str = env!("CARGO_PKG_NAME");
 static HOME_DIR: LazyLock<PathBuf> =
     LazyLock::new(|| UserDirs::new().unwrap().home_dir().to_path_buf());
 static CONFIG: RwLock<Option<Config>> = RwLock::new(None);
-static ENVIRONMENT: RwLock<Option<Environment>> = RwLock::new(None);
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
@@ -72,19 +74,29 @@ fn main() -> Result<(), anyhow::Error> {
 
     let cfg_loc = init_cfg_loc(&cli)?;
     load_config(&cfg_loc)?;
+    let env = Arc::new(Environment::new());
+    let ctx = Arc::new(DynamicContext {});
 
     let mut _watchers = None;
     match cli.command {
         Some(Commands::Watch { debounce }) => {
-            _watchers = Some(init_watcher(&cfg_loc, debounce)?);
+            _watchers = Some(init_watcher(&cfg_loc, debounce, env, ctx)?);
             wait_for_ctrl_c();
         }
         None => {
-            // TODO: Implement unwatched mode
+            render_all(env, ctx);
         }
     }
 
     Ok(())
+}
+
+fn resolve_tilde_s<'de, D>(deserializer: D) -> Result<PathBuf, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: PathBuf = Deserialize::deserialize(deserializer)?;
+    resolve_tilde(&s).map_err(serde::de::Error::custom)
 }
 
 fn resolve_tilde(path: &PathBuf) -> Result<PathBuf, anyhow::Error> {
@@ -94,6 +106,37 @@ fn resolve_tilde(path: &PathBuf) -> Result<PathBuf, anyhow::Error> {
         bail!("Config must be an absolute path, or relative to home (~)")
     }
     return Ok(path.clone());
+}
+
+impl Tpl {
+    fn src(&self) -> &Path {
+        &self.src
+    }
+
+    /// Subscribe this template's src for updates. The parent is subscribed and later
+    /// child events filtered to this template specifically, to cover a shortcoming of
+    /// inotify where file deletes->creations cause subscriptions to be lost
+    fn try_subscribe(&self, w: &mut INotifyWatcher) {
+        println!("Subscribing to: {:?}", self.src());
+        if let Err(err) = w
+            .watch(&self.src().parent().unwrap(), RecursiveMode::Recursive)
+            .with_context(|| format!("at path {:?}", self.src()))
+        {
+            log::error!("Subscription failed {:#}", err);
+        }
+    }
+
+    fn render(&self, env: &Environment, ctx: &DynamicContext) -> () {
+        // TODO impl
+        println!("Would have rendered: {:?}", self.src());
+    }
+}
+
+fn render_all(env: Arc<Environment<'static>>, ctx: Arc<DynamicContext>) {
+    let binding = CONFIG.read();
+    for tpl in binding.as_ref().unwrap().tpls.iter() {
+        tpl.render(&env, &ctx);
+    }
 }
 
 fn wait_for_ctrl_c() {
@@ -161,11 +204,6 @@ fn load_config(cfg_loc: &Path) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-// fn load_env() -> Result<(), anyhow::Error> {
-//     let mut env = Environment::new();
-//     env.set_loader(path_loader(&template_path));
-//     Ok(())
-// }
 // Other TODO:
 //   - Determine how to resolve data for context, can I plug this into minijinja or do I need to write it?
 //     (eg from ENV vars, CLI param slush, config slush, or specific value files)
@@ -173,10 +211,22 @@ fn load_config(cfg_loc: &Path) -> Result<(), anyhow::Error> {
 //     - Should they be global?
 //     - Should there be support for template specific ones?
 //   - How will conflicting keys be resolved? Should every source have a name and a prefix?
+// See https://github.com/mitsuhiko/minijinja/blob/main/examples/dynamic-context/src/main.rs
+
+#[derive(Debug)]
+struct DynamicContext;
+impl Object for DynamicContext {
+    fn get_value(self: &Arc<Self>, key: &minijinja::Value) -> Option<minijinja::Value> {
+        let _ = key;
+        None
+    }
+}
 
 fn init_watcher(
     cfg_loc: &Path,
     debounce: Duration,
+    env: Arc<Environment<'static>>,
+    ctx: Arc<DynamicContext>,
 ) -> Result<
     (
         Debouncer<INotifyWatcher, FileIdMap>,
@@ -184,30 +234,42 @@ fn init_watcher(
     ),
     anyhow::Error,
 > {
-    let binding = CONFIG.read();
-    let cfg = binding.as_ref().unwrap();
     // TODO: Nice to have: pretty printing subscribe, unsubscribe
+
+    let env1 = env.clone();
+    let ctx1 = ctx.clone();
     let watcher = Arc::new(Mutex::new(new_debouncer(
         debounce,
         None,
-        |res: Result<Vec<DebouncedEvent>, Vec<Error>>| match res {
+        move |res: Result<Vec<DebouncedEvent>, Vec<Error>>| match res {
             // TODO: Nice to have: re-render the precise files that changed only
-            Ok(e) => {}
+            Ok(e) => {
+                let binding = CONFIG.read();
+                let cfg = binding.as_ref().unwrap();
+                for e in e
+                    .iter()
+                    .filter(|e| e.kind.is_modify() || e.kind.is_create())
+                {
+                    for tpl in e
+                        .paths
+                        .iter()
+                        .filter_map(|p| cfg.tpls.iter().find(|s| p == s.src()))
+                    {
+                        tpl.render(&env1, &ctx1);
+                    }
+                }
+            }
             Err(e) => log::error!("Watcher failed {:?}", e),
         },
     )?));
+
     {
         let mut w = watcher.lock();
+        let binding = CONFIG.read();
+        let cfg = binding.as_ref().unwrap();
         for t in cfg.tpls.iter() {
-            let resolved = resolve_tilde(&t.src)?;
-            println!("Subscribing to: {:?}", resolved);
-            if let Err(err) = w
-                .watcher()
-                .watch(&resolved, RecursiveMode::NonRecursive)
-                .with_context(|| format!("at path {:?}", resolved))
-            {
-                log::error!("Subscription failed {:#}", err);
-            }
+            t.try_subscribe(w.watcher());
+            t.render(&env, &ctx);
         }
     }
     let w2 = watcher.clone();
@@ -215,39 +277,28 @@ fn init_watcher(
     // Note: inotify will unsubscribe from files that no longer exist. Some editors will delete then create
     // ergo, we actually need to watch the parent directory and filter for the file we want
     let cfg_loc_2 = cfg_loc.to_path_buf();
+    let env2 = env.clone();
+    let ctx2 = ctx.clone();
     let mut config_watcher = new_debouncer(
         debounce,
         None,
         move |res: Result<Vec<DebouncedEvent>, Vec<Error>>| match res {
             Ok(events) => {
-                for e in events.iter() {
-                    if !e.kind.is_modify() && !e.kind.is_create() {
-                        continue;
-                    }
-                    if !e.paths.contains(&cfg_loc_2) {
-                        return;
-                    }
+                for e in events
+                    .iter()
+                    .filter(|e| e.kind.is_modify() || e.kind.is_create())
+                    .filter(|e| e.paths.contains(&cfg_loc_2))
+                {
                     if let Err(e) = load_config(&cfg_loc_2) {
                         log::error!("Failed to load config {:#}", e);
                         return;
                     }
-
                     let binding = CONFIG.read();
-
                     let cfg = binding.as_ref().unwrap();
                     for t in cfg.tpls.iter() {
-                        let resolved = resolve_tilde(&t.src).unwrap();
-                        println!("Subscribing to: {:?}", resolved);
-                        if let Err(err) = w2
-                            .lock()
-                            .watcher()
-                            .watch(&resolved, RecursiveMode::NonRecursive)
-                            .with_context(|| format!("at path {:?}", resolved))
-                        {
-                            log::error!("Subscription failed {:#}", err);
-                        }
+                        t.try_subscribe(w2.lock().watcher());
+                        t.render(&env2, &ctx2);
                     }
-                    // TODO: You'll also want to trigger an render for those
                 }
             }
             Err(e) => log::error!("Watcher failed {:?}", e),
